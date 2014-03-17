9a71e784751e3b1a11fb3313eb0eb0d5c8b5d60c
Adrian <adrian_320@tlen.pl>
2014-03-17 17:43:54 +0100

    Initial App

---
+++ zioprojekt/wsgi.py
@@ -0,0 +1,7 @@
+from django.core.wsgi import get_wsgi_application
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zioprojekt.settings")
+
+from dj_static import Cling
+application = Cling(get_wsgi_application())
