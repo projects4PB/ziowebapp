fb72932e477b774ae304b62996a20c2de9432472
a-olszewski <adriols91@gmail.com>
2014-05-10 17:34:01 +0200

    Home application

--- zioprojekt/settings.py
+++ zioprojekt/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'rest_framework',
     'zioprojekt.webservice',
+    'home',
 )
 
 MIDDLEWARE_CLASSES = (
--- zioprojekt/urls.py
+++ zioprojekt/urls.py
@@ -4,9 +4,11 @@ from django.contrib import admin
 admin.autodiscover()
 
 from webservice.views import NotesListWS
+from home.views import HomeView
 
 urlpatterns = patterns(
     '',
+    url(r'^home/', HomeView.as_view()),
     url(r'^notes/', NotesListWS.as_view()),
     url(r'^admin/', include(admin.site.urls)),
 )
