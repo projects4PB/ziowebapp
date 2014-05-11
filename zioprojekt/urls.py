4089485704b60aaa1605c6cf81b272b1a6db571a
a-olszewski <adriols91@gmail.com>
2014-05-11 18:37:03 +0200

    wersja startowa

--- zioprojekt/urls.py
+++ zioprojekt/urls.py
@@ -3,13 +3,17 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from webservice.views import NotesListWS
 from home.views import HomeView
+
+from webservice.views import NotesListWS
+
+from accounts.views import ProfileView
 
 urlpatterns = patterns(
     '',
-    url(r'^home/', HomeView.as_view()),
+    url(r'^$', HomeView.as_view()),
     url(r'^notes/', NotesListWS.as_view()),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/profile', ProfileView.as_view()),
     url(r'^accounts/', include('registration.backends.default.urls')),
 )
