5546f99237fcc1916c625a1be0614e4b40e266a0
a-olszewski <adriols91@gmail.com>
2014-05-18 18:42:39 +0200

    popups

--- zioprojekt/offers/views.py
+++ zioprojekt/offers/views.py
@@ -1,4 +1,5 @@
 from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 
 from .models import Offer
 
@@ -7,3 +8,9 @@ class CreateOfferView(CreateView):
     model = Offer
     template_name = 'offers/create.html'
     success_url = '/'
+
+
+class OffersListView(ListView):
+    """Return proper offers list"""
+    model = Offer
+    template_name = 'offers/list.html'
--- zioprojekt/webservice/views.py
+++ zioprojekt/webservice/views.py
@@ -1,4 +1,6 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from zioprojekt.notes.models import Note
 
@@ -6,5 +8,10 @@ from .serializers import NoteSerializer
 
 
 class NotesListWS(ListAPIView):
-    queryset = Note.objects.all()
+    authentication_class = BasicAuthentication
+    permission_classes = (IsAuthenticated,)
     serializer_class = NoteSerializer
+
+    def get_queryset(self):
+        #return Note.objects.get_for_user(self.request.user)
+        return Note.objects.all()
